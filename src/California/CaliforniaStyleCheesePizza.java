cdec3d725f86d991627d622762e54d57868492b7
Gabriel Jayme <gabrielj_89@hotmail.com>
2021-10-20 14:53:46 -0300

    Add California Style Cheese pizza

--- src/California/CaliforniaStyleCheesePizza.java
+++ src/California/CaliforniaStyleCheesePizza.java
@@ -3,4 +3,15 @@ package California;
 import Factory.Pizza;
 
 public class CaliforniaStyleCheesePizza extends Pizza {
+    public CaliforniaStyleCheesePizza() {
+        name = "California Style Sauce and Cheese";
+        dough = "Extra Thinck Crust Dough";
+        sauce = "Barbecue Sauce";
+
+        toppings.add("M&Ms");
+    }
+
+    public void cut() {
+        System.out.println("Cutting the pizza into cicle slices");
+    }
 }
--- src/PizzaTestDriver.java
+++ src/PizzaTestDriver.java
@@ -1,3 +1,4 @@
+import California.CaliforniaPizzaStore;
 import Chicago.ChicagoPizzaStore;
 import Factory.Pizza;
 import Factory.PizzaStore;
@@ -7,11 +8,15 @@ public class PizzaTestDriver {
     public static void main(String[] args) {
         PizzaStore nyStore = new NYPizzaStore();
         PizzaStore chicagoStore = new ChicagoPizzaStore();
+        PizzaStore californiaStore = new CaliforniaPizzaStore();
 
         Pizza pizza = nyStore.orderPizza("cheese");
         System.out.println("Gabriel order a " + pizza.getName() + "\n");
 
         pizza = chicagoStore.orderPizza("cheese");
         System.out.println("Elvis order a " + pizza.getName() + "\n");
+
+        pizza = californiaStore.orderPizza("cheese");
+        System.out.println("Daniel order a " + pizza.getName() + "\n");
     }
 }
